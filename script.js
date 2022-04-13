bc2509639d85d5013e75c705aac303383c17f090
DVangosen <57511483+DVangosen@users.noreply.github.com>
2022-04-13 11:31:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    var principal = document.getElementById("principal").value;
+function compute() {
+  
 
-    var rate = document.getElementById("rate").value;
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rateInput").value;
+    var ap = r/100;
+    var t = document.getElementById("years").value;
+    var calculateYear = parseInt(new Date().getFullYear()) + parseInt(t);
 
-    var years = document.getElementById("years").value;
+    if (p == null || p == "") {
+        alert("Please fill 'Amount' to calculate Simple Iterest.");
+        document.getElementById("principal").focus();
+        return false;
+    } else if (p == 0 || p < 0){
+        alert("Please enter Principal amount greater than 0.");
+        document.getElementById("principal").focus();
+        return false;
+    }
 
-    var interest = principal * years * rate /100;
+    var total;
+    total = p * ap * parseInt(t)
 
-    var year = new Date().getFullYear()+parseInt(years);
-
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("demo").innerHTML = "If you deposit total <mark>" + p + "</mark>,<br>at an interest rate of <mark>" + r + "%</mark> per year,<br>You will receive an amount of <mark>" + total + "</mark>,<br>in the year <mark>" + calculateYear + "</mark>."
 }
-     
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
-
-    
-}
+  
